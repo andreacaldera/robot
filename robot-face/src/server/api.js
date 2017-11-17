99a8ca6fc6f0ef54fba9956da83f798fd1d595a9
Andrea Caldera <andrea.caldera@gmail.com>
2017-11-17 23:42:45 +0000

    minor fix

--- robot-face/src/server/api.js
+++ robot-face/src/server/api.js
@@ -5,7 +5,9 @@ export default () => {
   const router = express.Router();
 
   router.get('/play/abunai-shiatsu', (req, res, next) =>
+  // return res.sendStatus(202);
     player().play('./abunai-shiatsu.mp3', (err) => {
+      console.log('ERROR', err);
       if (err) return next(err);
       return res.sendStatus(202);
     })

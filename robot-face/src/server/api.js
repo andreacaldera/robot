79fea0bce3d8bdccbb92053f1743f18b44b274bb
Andrea Caldera <andrea.caldera@gmail.com>
2017-11-18 11:19:09 +0000

    sound api

--- robot-face/src/server/api.js
+++ robot-face/src/server/api.js
@@ -7,7 +7,6 @@ export default () => {
   router.get('/play/abunai-shiatsu', (req, res, next) =>
   // return res.sendStatus(202);
     player().play('./abunai-shiatsu.mp3', (err) => {
-      console.log('ERROR', err);
       if (err) return next(err);
       return res.sendStatus(202);
     })
